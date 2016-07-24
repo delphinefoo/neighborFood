61cca6f66c041677b337399bf4875cf9e30f3671
Delphine Foo-Matkin <delphinef@gmail.com>
2016-07-23 17:48:14 -0700

    Added segue for consumer view

--- NeighborFood/SecondViewController.swift
+++ NeighborFood/SecondViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var imageOne: UIImageView!
     
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
         
-        let navbarFont = UIFont(name: "Gill Sans", size: 17) ?? UIFont.systemFontOfSize(17)
+        //let navbarFont = UIFont(name: "Gill Sans", size: 17) ?? UIFont.systemFontOfSize(17)
         
-        UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: navbarFont, NSForegroundColorAttributeName:UIColor.whiteColor()]
+        //UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: navbarFont, NSForegroundColorAttributeName:UIColor.whiteColor()]
         
         // Get a reference to our posts
         let database = FIRDatabase.database()
@@ -64,6 +69,16 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        usernameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
     }
 
 }
